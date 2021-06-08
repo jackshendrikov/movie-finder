7d64e0bfd67960584be9767dbd453a7f138a96aa
Jack Shendrikov <jackshendrikov@gmail.com>
2021-06-08 19:25:33 +0300

    Add urls to movies

---
+++ movies/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Main Movie Logic
+    path('', views.main_page, name="main-page"),
+    path('register', views.register, name="register"),
+    path('all', views.all_movies, name='all'),
+    path('allseries', views.all_series, name='series'),
+    path('top', views.top_movies, name='top100'),
+    path('advsearch', views.advanced_search, name='advanced-search'),
+    path('genre', views.genre_page, name='genre-page'),
+    path('result', views.result_page, name='result'),
+]
