381ff8bf857a455930011780aa87dbb3c9716fd8
Jack Shendrikov <jackshendrikov@gmail.com>
2021-06-17 23:27:22 +0300

    Add login page

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     # Main Movie Logic
     path('', views.main_page, name="main-page"),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
     # path('register', views.register, name="register"),
     # path('all', views.all_movies, name='all'),
     # path('allseries', views.all_series, name='series'),
