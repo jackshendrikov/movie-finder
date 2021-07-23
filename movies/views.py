68855765c7189e68e42eb4b7741fc40f0203ac9a
Jack Shendrikov <jackshendrikov@gmail.com>
2021-07-24 00:56:25 +0300

    Upload new movies

--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,8 @@
 import difflib
-import json
 import pandas as pd
-from urllib.request import urlopen
 
 from django.contrib import messages
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
